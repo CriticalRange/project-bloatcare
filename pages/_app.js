894ca9e63eeb8a451c8253a4053ae7f0ac413c7e
CriticalRange <muhksgn@gmail.com>
2023-06-25 01:52:16 +0300

    e

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import "tailwindcss/tailwind.css";
 import "../styles/global.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { ThemeProvider } from "next-themes";
+
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -20,23 +20,33 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <h1>A website that has blogs in it</h1>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <div className="flex flex-row justify-around mt-0 p-10">
+        <section>
+          <h1 className="text-3xl leading-normal p-0 ml-10">
+            A website that has blogs in it
+          </h1>
+          <p className="text-base m-10">
+            {`A very "never thought of before" idea right?`}{" "}
+          </p>
+        </section>
+        <section
+          className={`${utilStyles.headingMd} p-7 border-2 border-black rounded-xl`}
+        >
+          <h2 className={utilStyles.headingLg}>Favorites</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>{title}</Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <div></div>
+      </div>
     </Layout>
   );
 }
