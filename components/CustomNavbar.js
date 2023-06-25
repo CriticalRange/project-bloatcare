3ed9c677ca2e9ae2e9c77ee706812c89fba89961
CriticalRange <muhksgn@gmail.com>
2023-06-25 16:50:12 +0300

    Icon switcher completed

--- components/CustomNavbar.js
+++ components/CustomNavbar.js
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { Button, Navbar } from "flowbite-react";
+import { Button, DarkThemeToggle, Flowbite, Navbar } from "flowbite-react";
 import Image from "next/image";
 import { siteTitle } from "./layout";
 import utilStyles from "../styles/utils.module.css";
@@ -14,40 +13,36 @@ export default function CustomNavbar() {
   const router = useRouter();
   const isHomePage = router.pathname === "/";
 
+  console.log(theme);
   const brandClickHandler = () => {
     isHomePage ? null : router.push("/");
   };
-
   return (
-    <Navbar>
-      <Navbar.Brand className="cursor-pointer" onClick={brandClickHandler}>
-        <Image
-          priority
-          src="/images/profile.jpg"
-          className={`${utilStyles.borderCircle} mr-3 h-6 sm:h-9`}
-          height={44}
-          width={44}
-          alt="Profile picture"
-        />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          {siteTitle}
-        </span>
-      </Navbar.Brand>
-      <div className="flex md:order-2 flex-row">
-        <div>
-          <FontAwesomeIcon
+    <Flowbite>
+      <Navbar>
+        <Navbar.Brand className="cursor-pointer" onClick={brandClickHandler}>
+          <Image
+            priority
+            src="/images/profile.jpg"
+            className={`${utilStyles.borderCircle} mr-3 h-6 sm:h-9`}
+            height={44}
+            width={44}
+            alt="Profile picture"
+          />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            {siteTitle}
+          </span>
+        </Navbar.Brand>
+        <div className="flex md:order-2 flex-row">
+          <DarkThemeToggle
+            className="mr-5"
             onClick={() => {
               theme === "light" ? setTheme("dark") : setTheme("light");
             }}
-            className="text-4xl mr-5 mt-1"
-            icon={theme === "light" ? faSun : faCloudMoon}
-            color={theme === "light" ? "black" : "white"}
           />
+          <Button>Login</Button>
         </div>
-
-        <Button>Login</Button>
-        <Navbar.Toggle />
-      </div>
-    </Navbar>
+      </Navbar>
+    </Flowbite>
   );
 }
