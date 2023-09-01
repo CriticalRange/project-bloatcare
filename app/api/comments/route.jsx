2ec1c51c8ad3df2ca9f158dc91810ccb2819cf39
CriticalRange <muhammedkosgen0608112@gmail.com>
2023-09-01 14:51:14 +0300

    testing3

--- app/api/comments/route.jsx
+++ app/api/comments/route.jsx
@@ -2,10 +2,7 @@ import { NextResponse } from "next/server";
 import { sqlConfig } from "../layout";
 const sql = require("mssql");
 
-export async function POST(req, res) {
-  try {
-    return NextResponse(req.body);
-  } catch (error) {
-    return NextResponse("error: ");
-  }
+export async function POST(request) {
+  const res = await request.json();
+  return NextResponse.json({ res });
 }
