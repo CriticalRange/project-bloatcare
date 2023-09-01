e42e74ec87a6aae5d7632c29e26e2f4c52890785
CriticalRange <muhammedkosgen0608112@gmail.com>
2023-09-02 00:44:19 +0300

    trying something else

--- app/api/route.jsx
+++ app/api/route.jsx
@@ -4,10 +4,6 @@ const sql = require("mssql");
 
 export async function GET(req, res) {
   try {
-    // make sure that any items are correctly URL encoded in the connection string
-    console.log("Connecting...");
-    await sql.connect(sqlConfig);
-    console.log("Connected!");
     const result = await sql.query`SELECT TOP (1000) [Display_Name]
     FROM [bloatcare].[dbo].[users]`;
     return new NextResponse(JSON.stringify(result));
