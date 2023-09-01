0893e3ad27671f85bfbf9dff91df2a9d4195239b
CriticalRange <muhammedkosgen0608112@gmail.com>
2023-09-01 14:59:23 +0300

    added env variables to vercel, need restart

--- app/api/comments/route.jsx
+++ app/api/comments/route.jsx
@@ -3,6 +3,8 @@ import { sqlConfig } from "../layout";
 const sql = require("mssql");
 
 export async function POST(request) {
-  const res = await request.json();
-  return NextResponse.json({ res });
+  const formData = await request.formData();
+  const name = formData.get("name");
+  const email = formData.get("email");
+  return NextResponse.json({ name, email });
 }
